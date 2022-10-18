e2c89104241b9e85288b946b0743ab03e15f92c0
tommywa-a <tomiwa95@gmail.com>
2022-10-18 14:06:04 +0100

    Page Routes, Navbar, Sign in and Sign Up pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Navbar from './Components/Navbar';
+import Explore from './Pages/Explore';
+import Offers from './Pages/Offers';
+import Profile from './Pages/Profile';
+import SignIn from './Pages/SignIn';
+import SignUp from './Pages/SignUp';
+import ForgotPassword from './Pages/ForgotPassword';
+
 function App() {
   return (
     <>
-      <h1>My App</h1>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Explore />} />
+          <Route path='/offers' element={<Offers />} />
+          <Route path='/profile' element={<SignIn />} />
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+        </Routes>
+        <Navbar />
+      </Router>
     </>
   );
 }
