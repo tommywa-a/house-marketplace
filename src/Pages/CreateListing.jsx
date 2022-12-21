188bf4bad357a04cbf82e96658bd1e2190256eec
tommywa-a <tomiwa95@gmail.com>
2022-12-21 12:27:18 +0100

    Quick Note & Change

--- src/Pages/CreateListing.jsx
+++ src/Pages/CreateListing.jsx
@@ -90,7 +90,6 @@ function CreateListing() {
      } else {
       geolocation.lat = latitude
       geolocation.lng = longitude
-      location = address
      }
 
     //  Store image in firebase
@@ -145,9 +144,9 @@ function CreateListing() {
       timestamp: serverTimestamp()
     }
 
+    formDataCopy.location = address
     delete formDataCopy.images
     delete formDataCopy.address
-    location && (formDataCopy.location = location)
     !formDataCopy.offer && delete formDataCopy.discountedPrice
 
     const docRef = await addDoc(collection(db, 'listings'), formDataCopy)
